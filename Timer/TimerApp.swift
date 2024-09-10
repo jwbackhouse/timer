c7424e3cfafa8ae5042d09e46f67232ae5743bda
James Backhouse <jwbackhouse@gmail.com>
2024-09-10 21:26:08 +0100

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by James Backhouse on 10/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
